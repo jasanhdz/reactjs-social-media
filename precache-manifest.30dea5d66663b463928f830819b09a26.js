f4f859a4a05132561b11629ccc980510280b0701
jasanhdz <jasan814@gmail.com>
2020-06-20 22:53:22 -0500

    Updates

---
+++ precache-manifest.30dea5d66663b463928f830819b09a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8e665b281b30fd3847329ef3f45c7b",
+    "url": "/reactjs-social-media/index.html"
+  },
+  {
+    "revision": "ad420368262f7c0c73f3",
+    "url": "/reactjs-social-media/static/css/main.324db224.chunk.css"
+  },
+  {
+    "revision": "1cf7ff800fadb2151248",
+    "url": "/reactjs-social-media/static/js/2.de161c3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-social-media/static/js/2.de161c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad420368262f7c0c73f3",
+    "url": "/reactjs-social-media/static/js/main.d675ee7f.chunk.js"
+  },
+  {
+    "revision": "4e44b716bd8160e84ecb",
+    "url": "/reactjs-social-media/static/js/runtime-main.8f7cb676.js"
+  }
+]);
